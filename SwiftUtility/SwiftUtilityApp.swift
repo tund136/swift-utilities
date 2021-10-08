b1cde137968498b03511b38ed06dea282507b996
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-08 10:30:52 +0700

    Create the project

---
+++ SwiftUtility/SwiftUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUtilityApp.swift
+//  SwiftUtility
+//
+//  Created by Danh Tu on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
